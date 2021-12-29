4ce76639f3d4250df8cb08b72d438afd45d3f6a8
samkim980 <simonkimani980@gmail.com>
2021-12-29 08:45:01 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div className = 'sidebar'>
+      <div className="sidebar-top">
+        <i class="logo fab fa-sketch"></i> 
+        <span className="brand">The App</span> 
+      </div>
+      <div className="sidebar-center">
+        <ul className="list">
+          <li className="list-item active">
+            <i class=" list-item-icon fas fa-home"></i>
+            <span className="list-item-text">Dashboard</span>
+          </li>
+          <li className="list-item">
+            <i class=" list-item-icon fas fa-search"></i>
+            <span className="list-item-text">Search</span>
+          </li>
+          <li className="list-item">
+            <i class=" list-item-icon fas fa-stream"></i>
+            <span className="list-item-text">Insight</span>
+          </li>
+          <li className="list-item">
+            <i class=" list-item-icon fas fa-book"></i>
+            <span className="list-item-text">Docs</span>
+          </li>
+          <li className="list-item">
+            <i class=" list-item-icon fas fa-users"></i>
+            <span className="list-item-text">Community</span>
+          </li>
+          <li className="list-item">
+            <i class=" list-item-icon fas fa-shopping-cart"></i>
+            <span className="list-item-text">Market</span>
+          </li>
+          <li className="list-item">
+            <i class=" list-item-icon fas fa-question-circle"></i>
+            <span className="list-item-text">Resources</span>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebar-bottom">
+        <div className="color-box dark"></div>
+        <div className="color-box night"></div>
+        <div className="color-box light"></div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
